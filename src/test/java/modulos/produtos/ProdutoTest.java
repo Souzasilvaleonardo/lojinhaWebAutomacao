1abb24f3eadd39fafb43c734eb421b611b732403
Souzasilvaleonardo <you@example.com>
2023-11-09 22:43:21 -0300

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/modulos/produtos/ProdutoTest.java
@@ -0,0 +1,112 @@
+package modulos.produtos;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import paginas.LoginPage;
+
+import java.time.Duration;
+
+@DisplayName("Testes Web no Modulo de Produtos")
+public class ProdutoTest {
+
+    private WebDriver navegador;
+
+    @BeforeEach
+    public void beforeEach() {
+        //Abrir o navegador
+        WebDriverManager.chromedriver().setup();
+        this.navegador = new ChromeDriver();
+
+        this.navegador.manage().window().maximize();
+
+        this.navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+
+        // Navegar para a página da Lojinha Web
+        this.navegador.get("http://165.227.93.41/lojinha-web/v2/");
+    }
+
+    @Test
+    @DisplayName("Nao e permitido resgistrar um produto igual a zero")
+    public void testNaoEPermitidoRegistrarProdutoComValorIgualAZero() {
+
+
+        // Fazer Login
+        String mensagemApresentada = new LoginPage(navegador)
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                .acessarOFormilarioDeAdicaoNovoProduto()
+                .informarNomeDoProduto("Macbook Pro")
+                .informarValorDoProduto("000")
+                .informarACorDoProduto("Preto")
+                .submeterFormularioDeAdicaoComErro()
+                .capturarMensagemApresentada();
+
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+
+    }
+
+    @Test
+    @DisplayName("Nao e permitido resgistrar um produto maior que 7.000")
+    public void testNaoEPermitidoRegistrarProdutoComValormaiorque7k() {
+
+        String mensagemApresentada = new LoginPage(navegador)
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                .acessarOFormilarioDeAdicaoNovoProduto()
+                .informarNomeDoProduto("Macbook Pro")
+                .informarValorDoProduto("700001")
+                .informarACorDoProduto("Preto")
+                .submeterFormularioDeAdicaoComErro()
+                .capturarMensagemApresentada();
+
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+
+    }
+
+    @Test
+    @DisplayName("Conseguir adcionar produtos que estejam no limite de 0,01")
+    public void testAdcionarProdutosQueEstejamNaFaixaDeUmCentavos() {
+        String mensagemApresentada = new LoginPage(navegador)
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                .acessarOFormilarioDeAdicaoNovoProduto()
+                .informarNomeDoProduto("Macbook Pro")
+                .informarValorDoProduto("001")
+                .informarACorDoProduto("Preto")
+                .submeterFormularioDeAdicaoComSucesso()
+                .capturarMensagemApresentada();
+
+        Assertions.assertEquals("Produto adicionado com sucesso", mensagemApresentada);
+
+    }
+
+    @Test
+    @DisplayName("Conseguir adcionar produtos que estejam no limite de 7.000")
+    public void testAdcionarProdutosQueEstejamNaFaixaDeSetMilReais() {
+        String mensagemApresentada = new LoginPage(navegador)
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                .acessarOFormilarioDeAdicaoNovoProduto()
+                .informarNomeDoProduto("Macbook Pro")
+                .informarValorDoProduto("700000")
+                .informarACorDoProduto("Cinza")
+                .submeterFormularioDeAdicaoComSucesso()
+                .capturarMensagemApresentada();
+        Assertions.assertEquals("Produto adicionado com sucesso", mensagemApresentada);
+
+    }
+
+    @AfterEach
+    public void afterEach() {
+
+        navegador.quit();
+    }
+
+}
---
+++ src/test/java/paginas/FormularioDeAdicaoDeProdutoPage.java
@@ -0,0 +1,47 @@
+package paginas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class FormularioDeAdicaoDeProdutoPage {
+    private WebDriver navegador;
+
+    public FormularioDeAdicaoDeProdutoPage(WebDriver navegador) {
+        this.navegador = navegador;
+    }
+
+    public FormularioDeAdicaoDeProdutoPage informarNomeDoProduto(String produtoNome) {
+        navegador.findElement(By.id("produtonome")).sendKeys(produtoNome);
+
+        return this;
+    }
+
+    public FormularioDeAdicaoDeProdutoPage informarValorDoProduto(String produtoValor) {
+        navegador.findElement(By.id("produtovalor")).sendKeys(produtoValor);
+
+        return this;
+    }
+
+    public FormularioDeAdicaoDeProdutoPage informarACorDoProduto(String produtoCor) {
+        navegador.findElement(By.id("produtocores")).sendKeys(produtoCor);
+
+        return this;
+    }
+
+    public ListaDeProdutosPage submeterFormularioDeAdicaoComErro() {
+        navegador.findElement(By.cssSelector("button[type='submit']")).click();
+
+        return new ListaDeProdutosPage(navegador);
+    }
+
+    public FormularioDeEdicaoDeProdutoPage submeterFormularioDeAdicaoComSucesso() {
+        navegador.findElement(By.cssSelector("button[type='submit']")).click();
+
+        return new FormularioDeEdicaoDeProdutoPage(navegador);
+    }
+
+
+
+    //<div class="toast rounded" style="top: 0px; transition: transform 0.2s ease 0s, opacity 0.2s ease 0s;">Produto adicionado com sucesso</div>
+
+}
